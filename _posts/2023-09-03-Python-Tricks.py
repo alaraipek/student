37e868586180402d6ff310bd9cc9103ec22a7d1c
alaraipek <alarai138@icloud.com>
2023-09-04 13:29:37 -0700

    t

---
+++ _posts/2023-09-03-Python-Tricks.py
@@ -0,0 +1,33 @@
+from emoji import emojize 
+print(emojize(":thumbs_up: Python is awesome! :grinning_face:"))
+
+from newspaper import Article
+from IPython.display import display, Markdown
+
+
+urls = ["http://cnn.com/2023/03/29/entertainment/the-mandalorian-episode-5-recap/index.html", 
+        "https://www.cnn.com/2023/06/09/entertainment/jurassic-park-anniversary/index.html"]
+
+for url in urls:
+    article = Article(url)
+    article.download()
+    article.parse()
+    # Jupyter Notebook Display
+    # print(article.title)
+    display(Markdown(article.title)) # Jupyter display only
+    display(Markdown(article.text)) # Jupyter display only
+    print("\n")
+
+    import wikipedia 
+from IPython.display import display, Markdown # add for Jupyter
+
+terms = ["Python (programming language)", "JavaScript"]
+for term in terms:
+    # Search for a page 
+    result = wikipedia.search(term)
+    # Get the summary of the first result
+    summary = wikipedia.summary(result[0])
+    print(term) 
+    # print(summary) # console display
+    display(Markdown(summary)) # Jupyter display
+    
